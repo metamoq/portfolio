b5d6115b6d6484237d91f8274e0ae019e5e7dd2a
Metamoq <werewolf13@list.ru>
2023-09-13 12:45:24 +0300

    Your commit message here

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 import os
 import openai
+import subprocess
+import threading
 
 app = Flask(__name__, template_folder='templates', static_url_path='/static', static_folder='static')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-# Установите ваш API-ключ от OpenAI
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 model = "gpt-3.5-turbo"  # Выбираем подходящую модель
+
+# Список сообщений для хранения истории беседы
+chat_history = []
+
+lama_app_started = False
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/image1')
 def image1():
     return render_template('image1.html')
+
 
 @app.route('/image2')
 def image2():
     return render_template('image2.html')
 
+
 @app.route('/image3')
 def image3():
-    return render_template('image3.html')
+    return redirect('http://127.0.0.1:7860/image3')
 
 @app.route('/generate-response', methods=['POST'])
 def generate_response():
@@ -45,14 +54,33 @@ def generate_response():
             ]
         )
         model_response = response['choices'][0]['message']['content']
-        return jsonify({'response': model_response})
+
+        # Добавляем сообщение в историю беседы
+        chat_history.append({"role": "user", "content": user_input})
+        chat_history.append({"role": "assistant", "content": model_response})
+
+        # Возвращаем историю беседы клиенту
+        return jsonify({'response': model_response, 'chat_history': chat_history})
     except Exception as e:
         print("Error:", e)
         return jsonify({'error': str(e)}), 500
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return jsonify(error=str(e)), 500
 
+@app.route('/gradio-app')
+def gradio_app():
+    return redirect(url_for('gradio_interface'))
+
+
 if __name__ == '__main__':
+    # Запустите Lama.py в отдельном потоке при запуске основного приложения
+    if not lama_app_started:
+        lama_thread = threading.Thread(target=subprocess.Popen, args=(["python", "Lama.py"],))
+        lama_thread.start()
+        lama_app_started = True
+
+    # Запустите основное приложение app.py
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,5 +1,3 @@
-// main.js
-
 $(document).ready(function() {
     $('#user-input-form').submit(function(event) {
         event.preventDefault();
@@ -14,10 +12,19 @@ $(document).ready(function() {
             success: function(data) {
                 // Обновляем содержимое элемента с id "model-response" данными, полученными от сервера
                 $('#model-response').text(data.response);
+
+                // Добавляем ответ модели в историю беседы
+                var chatHistory = $('#chat-history');
+                var userMessage = $('<p class="user-message"></p>').text(userInput);
+                var modelMessage = $('<p class="model-message"></p>').text(data.response);
+                chatHistory.append(userMessage, modelMessage);
             },
             error: function(error) {
                 console.error('Произошла ошибка:', error);
             }
         });
+
+        // Очищаем поле ввода после отправки запроса
+        $('#user-input').val('');
     });
 });
